f3626210147d4c4f07d608d869aff929f768b512
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-10 16:21:30 +0200

    finish inactive and active pages

--- src/app/pages/active-posts/active-posts.component.ts
+++ src/app/pages/active-posts/active-posts.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { iPost } from '../../Modules/i-post';
+import { iJsonResponse } from '../../Modules/i-json-response';
 
 @Component({
   selector: 'app-active-posts',
   templateUrl: './active-posts.component.html',
-  styleUrl: './active-posts.component.scss'
+  styleUrl: './active-posts.component.scss',
 })
 export class ActivePostsComponent {
+  activePost!: iPost[];
+  shuffleArray(array: iPost[]): iPost[] {
+    let currentIndex = array.length,
+      randomIndex;
 
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  ngOnInit() {
+    fetch('db.json')
+      .then((res) => <Promise<iJsonResponse>>res.json())
+      .then((res) => {
+        const postsArr: iPost[] = res.posts;
+        const postsArrShuffled = this.shuffleArray(postsArr);
+        this.activePost = postsArrShuffled.filter((p) => p.active);
+        console.log(this.activePost);
+      });
+  }
 }
--- src/app/pages/inactive-posts/inactive-posts.component.ts
+++ src/app/pages/inactive-posts/inactive-posts.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { iPost } from '../../Modules/i-post';
+import { iJsonResponse } from '../../Modules/i-json-response';
 
 @Component({
   selector: 'app-inactive-posts',
   templateUrl: './inactive-posts.component.html',
-  styleUrl: './inactive-posts.component.scss'
+  styleUrl: './inactive-posts.component.scss',
 })
 export class InactivePostsComponent {
+  inactivePost!: iPost[];
+  shuffleArray(array: iPost[]): iPost[] {
+    let currentIndex = array.length,
+      randomIndex;
 
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  ngOnInit() {
+    fetch('db.json')
+      .then((res) => <Promise<iJsonResponse>>res.json())
+      .then((res) => {
+        const postsArr: iPost[] = res.posts;
+        const postsArrShuffled = this.shuffleArray(postsArr);
+        this.inactivePost = postsArrShuffled.filter((p) => !p.active);
+        console.log(this.inactivePost);
+      });
+  }
 }
